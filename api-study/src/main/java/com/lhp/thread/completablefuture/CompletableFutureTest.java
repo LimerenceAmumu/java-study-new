c3059ef5ef31aea614725e0e7904172318ddf967
Amumu <2210074693@qq.com>
2022-07-26 20:13:12 +0800

    啊

--- api-study/src/main/java/com/lhp/thread/completablefuture/CompletableFutureTest.java
+++ api-study/src/main/java/com/lhp/thread/completablefuture/CompletableFutureTest.java
@@ -1,9 +1,6 @@
 package com.lhp.thread.completablefuture;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 
 /**
  * @Description:
@@ -25,6 +22,25 @@ public class CompletableFutureTest {
         MedalInfo medalInfo = completableMedalInfoFuture.get();//获取勋章信息结果
         System.out.println("总共用时" + (System.currentTimeMillis() - startTime) + "ms");
 
+        //****************分隔 和上面没关联*************************
+        CompletableFuture<Integer> uCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            int i = ThreadLocalRandom.current().nextInt(100);
+            System.out.println("i = " + i);
+            if (i > 2) {
+                i = i / 0;
+            }
+            return i;
+        });
+        uCompletableFuture.whenComplete((v, e) -> {
+            //必须要判断，因为 无论上一步任务是否发生异常都会进入到这里，e!=null 时 返回值是null
+            if(e==null){
+                System.out.println("获取到的上一步计算结果：" + v);
+            }
+        }).exceptionally((e) -> {
+            System.out.println("发生了异常！");
+            e.printStackTrace();
+            return null;
+        });
     }
 
 }
--- api-study/src/main/java/com/lhp/thread/runnable/LongTimeTask.java
+++ api-study/src/main/java/com/lhp/thread/runnable/LongTimeTask.java
@@ -6,10 +6,9 @@ package com.lhp.thread.runnable;
  * @date: 2021/9/8 1:58 下午
  */
 public class LongTimeTask implements Runnable {
-
-
     @Override
     public void run() {
 
+        //do sth
     }
 }
